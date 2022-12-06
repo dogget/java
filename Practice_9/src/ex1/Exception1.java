5567101d9408c6960aff018101eb2e232bbf9f33
dogget <55401978+dogget@users.noreply.github.com>
2022-12-06 16:44:32 +0300

    practice 18

---
+++ Practice_9/src/ex1/Exception1.java
@@ -0,0 +1,14 @@
+package Exception1;
+public class Exception1 {
+    public static void exceptionDemo() {
+        try {
+            System.out.println(2 / 0);
+        } catch (ArithmeticException exception) {
+            System.out.println("Attempted division by zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        exceptionDemo();
+    }
+}
---
+++ Practice_9/src/ex3/Exception3.java
@@ -0,0 +1,19 @@
+package Exception3;
+import java.util.Scanner;
+
+public class Exception3 {
+    public static void exceptionDemo() {
+        Scanner sc = new Scanner(System.in);
+        String intString = sc.next();
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        }catch (Exception e) {
+            System.out.println("Any Error");
+        }
+    }
+
+    public static void main(String[] args) {
+        exceptionDemo();
+    }
+}
